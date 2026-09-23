22b7880c76a2d4e2999547de58dd83e1b35277f4
unnatieee <i26ai029@aid.svnit.ac.in>
2026-09-23 11:13:27 +0530

    Add files via upload

---
+++ Q32.c
@@ -0,0 +1,42 @@
+#include <stdio.h>                         
+
+int main()
+{
+    int n, i, x;                           
+    int max, second;                      
+
+    printf("Enter N: ");                   
+    scanf("%d", &n);                       
+
+    printf("Enter first number: ");        
+    scanf("%d", &max);                     
+
+    printf("Enter second number: ");       
+    scanf("%d", &second);                  
+
+    if(second > max)                      
+    {
+        x = max;                          
+        max = second;                    
+        second = x;                        
+    }
+
+    for(i = 3; i <= n; i++)                
+    {
+        printf("Enter number: ");          
+        scanf("%d", &x);                   
+        if(x > max)                       
+        {
+            second = max;                  
+            max = x;                       
+        }
+
+        else if(x > second && x != max)    
+            second = x;                    
+    }
+
+    printf("Maximum = %d\n", max);         
+    printf("Second Maximum = %d", second); 
+
+    return 0;                              
+}
